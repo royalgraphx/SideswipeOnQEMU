133bafc78efd51e088ed9c71ae717921741951c1
rgx <rgx@rgx.com>
2022-08-28 17:05:13 -0500

    Initial ReleaseReRelease

--- preinstall.sh
+++ preinstall.sh
@@ -16,7 +16,7 @@ sudo apt-get install libvde-dev libvdeplug-dev libvte-2.91-dev libxen-dev liblzo
 sudo apt-get install valgrind xfslibs-dev
 sudo apt-get install libnfs-dev libiscsi-dev
 sudo apt-get install binwalk p7zip-full
-sudo apt-get install qemu-utils
+sudo apt-get install qemu-utils adb
 sudo apt-get install libusb-1.0-0-dev
 sudo apt-get install git-email libaio-dev libbluetooth-dev libcapstone-dev libbrlapi-dev libbz2-dev libcap-ng-dev libcurl4-gnutls-dev libgtk-3-dev libibverbs-dev libjpeg8-dev libncurses5-dev libnuma-dev librbd-dev librdmacm-dev libsasl2-dev libsdl2-dev libseccomp-dev libsnappy-dev libssh-dev libvde-dev libvdeplug-dev libvte-2.91-dev libxen-dev liblzo2-dev valgrind xfslibs-dev libnfs-dev libiscsi-dev build-essential libepoxy-dev libdrm-dev libgbm-dev libx11-dev libvirglrenderer-dev libpulse-dev libsdl2-dev libgtk-3-dev
 
