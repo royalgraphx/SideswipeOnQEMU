4caa890c74c19836acd4c9aa75751bc800cc20e1
RGX <39929362+royalgraphx@users.noreply.github.com>
2022-11-24 06:56:43 -0600

    convert to arch format

--- preinstall.sh
+++ preinstall.sh
@@ -4,11 +4,12 @@
 git_dir=$HOME/Desktop/SideswipeOnQEMU
 
 # install required dependencies
-sudo apt-get update
-sudo apt-get install -y git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build git-email libaio-dev libbluetooth-dev libcapstone-dev libbrlapi-dev libbz2-dev libcap-ng-dev libcurl4-gnutls-dev libgtk-3-dev libibverbs-dev libjpeg8-dev libncurses5-dev libnuma-dev librbd-dev librdmacm-dev libsasl2-dev libsdl2-dev libseccomp-dev libsnappy-dev libssh-dev libvde-dev libvdeplug-dev libvte-2.91-dev libxen-dev liblzo2-dev valgrind xfslibs-dev libnfs-dev libiscsi-dev binwalk p7zip-full qemu-utils adb libusb-1.0-0-dev build-essential libepoxy-dev libdrm-dev libgbm-dev libx11-dev libvirglrenderer-dev libpulse-dev libsdl2-dev libgtk-3-dev
+sudo pacman -Syu
+
+sudo pacman -S git glib2 dtc pixman zlib ninja libaio bluez-libs capstone brltty bzip2 libcap-ng libcurl-gnutls gtk3 libjpeg-turbo ncurses numactl libsasl sdl2 libseccomp snappy libssh libusb vte3 lzo valgrind libnfs libiscsi binwalk p7zip qemu-tools android-tools base-devel libepoxy libdrm mesa libx11 virglrenderer libpulse
 
 # adding user to kvm group, and setting udev rule
-sudo adduser $USER kvm
+sudo usermod -a -G kvm,libvirt $(whoami)
 sudo cp $git_dir/99-kvm.rules "/lib/udev/rules.d/"
 
 # set udev rule for controller passthrough
