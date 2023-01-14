940b0ec8e2b67ba6ac6da4520fc4cfe6619df35f
RoyalGraphX <rgx@royalgraphx.live>
2023-01-14 10:09:55 -0600

    Create ISO automation

---
+++ iso/fetch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# fetch.sh
+
+# Welcome to the ISO Section of the autodeploy script! Created by RoyalGraphX on Jan 14, 2023
+# Automation for retrieval of BlissRom for use in sideswipe-vm/SideswipeOnQEMU.
+# No harm intended on Mediafire, moving to a better hosting solution soon.
+# ~ RoyalGraphX
+
+curl -O "https://www.mediafire.com/file/g7qh0l4z6lqj6hk/Bliss-v11.13--OFFICIAL-20201113-1525_x86_64_k-k4.19.122-ax86-ga-rmi_m-20.1.0-llvm90_dgc-t3_gms_intelhd.iso/file"
+grep -o 'http[s]*://download[^/][^\\]*' -m 1 file > url
+sed 's/"           id="downloadButton"//g' url > url_strip
+wget "$(cat url_strip)"
+echo Complete! Continuing ...
--- iso/readme.txt
+++
@@ -1,3 +0,0 @@
-please download and place Bliss OS 11.13 ISO from the link below into this folder.
-
-https://www.mediafire.com/file/g7qh0l4z6lqj6hk/Bliss-v11.13--OFFICIAL-20201113-1525_x86_64_k-k4.19.122-ax86-ga-rmi_m-20.1.0-llvm90_dgc-t3_gms_intelhd.iso/file
