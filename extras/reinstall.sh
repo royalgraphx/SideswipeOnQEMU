7842d2a96ddb1f6ebe7cb9bdd7efa5811a62665e
RoyalGraphX <rgx@royalgraphx.live>
2023-01-14 18:35:48 -0600

    Finalize Reinstall Script

--- extras/reinstall.sh
+++ extras/reinstall.sh
@@ -8,4 +8,18 @@ android_dir=$sudo_user_home/Desktop/sideswipe-vm
 # sudo rm -rf $qemu_dir
 sudo rm -rf $android_dir
 
+echo $android_dir
 echo Done!
+sleep 1
+clear
+echo Begin Installation...
+echo
+sleep 1
+echo Enter Distro Type:
+echo Debian=0, Arch=1
+read distro
+if [[ $distro > 0 ]]; then
+./preinstall_arch.sh
+else
+./preinstall_debian.sh
+fi
